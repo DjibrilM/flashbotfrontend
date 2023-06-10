3060a12c78bf7409a15d8f5851565335de16ff9f
Djibril-Mugisho <mugishodjibril2004@gmail>
2023-06-11 01:35:14 +0300

    Added switch button

---
+++ src/components/switch/Switch.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+const Switch = () => {
+    const [active, setActive] = useState<boolean>(true);
+
+    return <div className="h-10 overflow-hidden w-[6rem] p-2 relative border-[#ffffff34] rounded-full border bg-[#00000027]">
+        <div style={active ? { marginLeft: "3.3rem", } : { marginRight: "1rem" }} className="w-6 h-6  border-[#ffffff46] border-2 duration-300 cursor-pointer  rounded-full bg-purple-500 absolute z-20">
+        </div>
+
+        <div className="absolute font-thin z-0 w-full h-full top-0  items-center px-2 left-0 flex justify-between">
+            <p onClick={() => setActive(false)} className=" cursor-pointer pl-1 text-[10px]">OFF</p>
+            <p onClick={() => setActive(true)} className="mr-1 cursor-pointer text-[10px]">ON</p>
+        </div>
+    </div>;
+};
+
+export default Switch;
