5ad47a9362be3dff8f3c610789f7f87cebbc2e04
Djibril-Mugisho <mugishodjibril2004@gmail>
2023-06-12 23:41:26 +0300

    Added message component

--- src/routes/chatDetail/ChatDetail.tsx
+++ src/routes/chatDetail/ChatDetail.tsx
@@ -1,5 +1,13 @@
+import { useEffect } from "react";
 import ChatArea from "../../components/forms/ChatArea";
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+import ResponseMessage from "../../components/response-message/response-message";
+import { createRipples } from "react-ripples";
+const RippleButton = createRipples({
+  color: "#ffffff0b",
+  during: 600,
+  className: "rounded-md "
+})
 
 
 
@@ -13,21 +21,49 @@ const ChatDetail = () => {
     browserSupportsSpeechRecognition,
   } = useSpeechRecognition();
 
+
+  const createSpeech = () => {
+    try {
+      let utterance = new SpeechSynthesisUtterance(transcript);
+      const voice = window.speechSynthesis.getVoices()[3];
+      utterance.voice = voice;
+      speechSynthesis.speak(utterance);
+    } catch (error) {
+      console.log(error)
+    }
+  };
+
+
   return <>
-    <div className=" h-full rounded-lg p-5 w-full bg-[#ffffff0b] text-white ">
-      <p>Microphone: {listening ? 'on' : 'off'}</p>
-      <br />
-      <button onClick={() => resetTranscript()}>Reset</button>
-      <p>{transcript}</p>
+    <div className=" h-full overflow-auto rounded-lg flex flex-col p-5 w-full  text-white ">
+      {listening ?
+        <>
+          <div className="h-full w-full">
+            <p className="text-gray-200">{transcript}</p>
+          </div>
+          <div className=" w-full pt-2 justify-between   border-[#ffffff1d] flex items-center  border-t">
+            <RippleButton>
+              <button onClick={() => resetTranscript()} className="px-20 rounded-md text-sm bg-[#ffffff0b] py-3">
+                Reset
+              </button>
+            </RippleButton>
+
+            <p>{transcript.split('').length} / 100</p>
+          </div>
+        </> : <>
+          <ResponseMessage />
+        </>}
     </div>
     <ChatArea
       onStopRecording={() => {
-        SpeechRecognition.stopListening()
+        SpeechRecognition.stopListening();
+        createSpeech();
+        resetTranscript();
       }}
       onStartRecording={() => {
         SpeechRecognition.startListening({
           continuous: true,
-          language: 'fr-FR'
+          language: 'en-US'
         })
       }} listening={listening} />
   </>
